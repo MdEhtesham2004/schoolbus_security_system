3f8e2d1faafe9c816b13060db0c32540efb1e170
md ehtesham <ehteshammd089@gmail.com>
2025-03-26 07:46:18 +0530

    procfile

--- School_Bus_Safety_System/School_Bus_Safety_System/settings.py
+++ School_Bus_Safety_System/School_Bus_Safety_System/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8ja#-3-7go*(n=@phof#xq-kt&!r8jc4wsjvlsqzb$pj2cpwb%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['schoolbus_security_system.onrender.com']
 
 
 # Application definition
